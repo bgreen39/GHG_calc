3d126a5ef7509c9229909d00feed4f92cfb099a4
Brianna <70491376+bgreen39@users.noreply.github.com>
2025-03-20 22:32:25 -0500

    Update and rename co2_emissions_calc.py to electrical_emissions.py

--- calculator/co2_emissions_calc.py
+++
@@ -1,4 +0,0 @@
-# Calculations for CO2
-def calculate_emissions(electricity_mwh):
-co2_emissions = electricity_mwh * 0.9
-
---
+++ calculator/electrical_emissions.py
@@ -0,0 +1,13 @@
+# Calculations for GHG emissions from Electrical Consumption
+
+# Define calculation emissions for electrical consumption.
+# Make sure to include calculations for these different units: kilowatt hours and megawatt hours
+# Note: In guidebook note that users can request units to be added. Look up EFs to use for the ghg emissions calculations.
+
+# Carbon Dioxide (CO2) Emissions
+
+# Methane (CH4) Emissions
+# *create definition for this function*
+
+# Nitrous Oxide (N2O) Emissions
+# *create definition for this function*
