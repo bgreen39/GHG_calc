e372908452f2dbea6e78347a9cc04c567af909ed
Brianna <70491376+bgreen39@users.noreply.github.com>
2025-03-26 23:35:58 -0500

    Update res_ele_emissions.py

--- calculator/res_ele_emissions.py
+++ calculator/res_ele_emissions.py
@@ -5,6 +5,19 @@
 # Note: In guidebook note that users can request units to be added. Look up EFs to use for the ghg emissions calculations.
 
 # Carbon Dioxide (CO2) Emissions
+def electricity_emissions(value, unit="kwh","mwh",output_unit="metric_tons","tons"):
+
+#Emissions factor (ef) from kg co2e per kwh
+ef=0.4
+#conversion from mwh to kwh
+if unit == "mwh":
+  value *= 1000 
+
+#calculate emissions in kg
+emissions_kg = value * ef
+emissions_metric_tons = emissions_kg / 1000
+
+
 
 # Methane (CH4) Emissions
 # *create definition for this function*
