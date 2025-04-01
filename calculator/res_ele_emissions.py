57f1d241941fe776038e854d517e1b327ae43239
Brianna <70491376+bgreen39@users.noreply.github.com>
2025-03-31 23:40:02 -0500

    Update res_ele_emissions.py
    Added kg co2 per Mwh emissions factors for each ghg emission. <3

--- calculator/res_ele_emissions.py
+++ calculator/res_ele_emissions.py
@@ -5,22 +5,39 @@
 # Note: In guidebook note that users can request units to be added. Look up EFs to use for the ghg emissions calculations.
 
 # Carbon Dioxide (CO2) Emissions
-def electricity_emissions(value, unit="kwh","mwh",output_unit="metric_tons","tons"):
-
+def electricity_emissions_co2(value, unit="kwh","mwh",output_unit="metric_tons","tons"):
 #Emissions factor (ef) from kg co2e per kwh
-ef=0.4
+#     *updated from excel (kg co2 per Mwh)
+co2_EF= 352
 #conversion from mwh to kwh
 if unit == "mwh":
   value *= 1000 
-
 #calculate emissions in kg
 emissions_kg = value * ef
 emissions_metric_tons = emissions_kg / 1000
 
-
-
-# Methane (CH4) Emissions
+# METHANE (CH4) Emissions
 # *create definition for this function*
+def electricity_emissions_ch4(value, unit="kwh","mwh",output_unit="metric_tons","tons"):
+#Emissions factor (ef) from kg co2e per kwh
+#     *updated from excel (kg co2 per Mwh)
+ch42_EF= 0.023
+#conversion from mwh to kwh
+if unit == "mwh":
+  value *= 1000 
+#calculate emissions in kg
+emissions_kg = value * ef
+emissions_metric_tons = emissions_kg / 1000
 
 # Nitrous Oxide (N2O) Emissions
 # *create definition for this function*
+def electricity_emissions_n2o(value, unit="kwh","mwh",output_unit="metric_tons","tons"):
+#Emissions factor (ef) from kg co2e per kwh
+#     *updated from excel (kg co2 per Mwh)
+n2o_EF= 0.003
+#conversion from mwh to kwh
+if unit == "mwh":
+  value *= 1000 
+#calculate emissions in kg
+emissions_kg = value * ef
+emissions_metric_tons = emissions_kg / 1000
