5fba938daecdd522acbda21c6a1a9151ace1c03e
Brianna <70491376+bgreen39@users.noreply.github.com>
2025-04-07 23:49:58 -0500

    Update residential_fuel_emissions.py
    changed the carbon diozide emissions

--- calculator/residential_fuel_emissions.py
+++ calculator/residential_fuel_emissions.py
@@ -2,16 +2,16 @@
 # Include units for thousand cubic feet, and therms 
 
 # Carbon Dioxide (CO2) Emissions
-def gas_emissions_co2(value, unit="kwh","mwh",output_unit="metric_tons","tons"):
-#Emissions factor (ef) from kg co2e per kwh
-#     *updated from excel (kg co2 per Mwh)
-co2_EF= 352
-#conversion from mwh to kwh
-if unit == "mwh":
-  value *= 1000 
-#calculate emissions in kg
-emissions_kg = value * ef
-emissions_metric_tons = emissions_kg / 1000
+def gas_emissions_co2(value, unit="kwh", output_unit="metric_tons"):
+    # Emissions factor in kg CO2 per kWh (352 kg/MWh = 0.352 kg/kWh)
+    co2_EF = 0.352
+
+    if unit == "mwh":
+        value *= 1000  # convert MWh to kWh
+
+    emissions_kg = value * co2_EF
+    emissions = emissions_kg / 1000 if output_unit == "metric_tons" else emissions_kg * 0.00110231  # to short tons
+    return emissions
 
 # METHANE (CH4) Emissions
 # *create definition for this function*
@@ -38,3 +38,5 @@ if unit == "mwh":
 #calculate emissions in kg
 emissions_kg = value * ef
 emissions_metric_tons = emissions_kg / 1000
+
+
